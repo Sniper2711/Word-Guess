05821a09bf7cb67e7114926975b304cec8662f13
Sniper2711 <127583685+Sniper2711@users.noreply.github.com>
2024-07-31 14:20:02 +0800

    Add files via upload

---
+++ Word-Guess.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef ╡б╢й_PRIVATE_H
+#define ╡б╢й_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.2.0.47"
+#define VER_MAJOR	1
+#define VER_MINOR	2
+#define VER_RELEASE	0
+#define VER_BUILD	47
+#define COMPANY_NAME	""
+#define FILE_VERSION	"1.2.0.47"
+#define FILE_DESCRIPTION	"╡б╢йсно╥"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"╡б╢йсно╥"
+#define PRODUCT_VERSION	"1.2.0.47"
+
+#endif /*╡б╢й_PRIVATE_H*/
