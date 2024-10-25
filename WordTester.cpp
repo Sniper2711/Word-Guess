168bb3d1716813dbd0d6e926358ab9f25f599aa9
Sniper2711 <hsf18_197000@163.com>
2024-10-25 18:35:00 +0800

    Update WordTester

--- WordTester.cpp
+++ WordTester.cpp
@@ -9,14 +9,27 @@ int main() {
 	ofstream ofs;
 	ifs.open(filein, ios::in);
 	ofs.open(fileout, ios::out);
-	while(getline(ifs, temp)) {
+	if(!ifs.is_open()) return 1;
+	if(!ofs.is_open()) return 2;
+	while(true) {
 		flag = true;
-		if(temp.length() <= 4 || temp.length() >= 16) {
-			cout << temp << endl;
+		getline(ifs, temp);
+		string wd, mn;
+		temp.erase(0, temp.find_first_not_of(" \t"));
+		temp.erase(temp.find_last_not_of(" \t") + 1);
+		for(int i = 0;i < temp.length();i++) {
+			if(temp[i] == ' ') {
+				wd = temp.substr(0, i);
+				mn = temp.substr(i+1);
+				break;
+			}
+		} 
+		if(wd.length() <= 5 || wd.length() >= 16) {
+			cout << wd << endl;
 			flag = false;
 		}
-		for(char c : temp) {
-			if(c != '-' && c != '\'' && !isalpha(c)) {
+		for(char c : wd) {
+			if(c != '-' && !isalpha(c)) {
 				cout << temp << endl;
 				flag = false;
 				break;
